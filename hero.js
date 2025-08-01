ac87aea9b3cafc1628c75d6506bc59d5a5851a6a
ZataMod <vinhmod4@gmail.com>
2025-08-01 15:30:36 +0700

    Thêm hero: WAVE

--- hero.js
+++ hero.js
@@ -1,1 +1,9 @@
-const heroes = [];
+const heroes = [
+  {
+    "img": "https://lh3.googleusercontent.com/d/1N69hEo6-jAxFxK2TLPHWTUqrAuOrV0sH",
+    "name_hero": "WAVE",
+    "title": "FULL WAVE + PHU KIEN",
+    "video": "https://www.youtube.com/embed/TMsptx28XUY",
+    "link": "https://link4m.com/G5PfbB"
+  }
+];
