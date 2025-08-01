98b4ea21c79a248154d430ca93a398e8e923c59b
ZataMod <vinhmod4@gmail.com>
2025-08-02 00:10:32 +0700

    Thêm hero: LINH

--- hero.js
+++ hero.js
@@ -1,5 +1,12 @@
 const heroes = [
   {
+    "img": "https://lh3.googleusercontent.com/d/1UVna0fjwVW23dgqx1qV_kD26105KfnsW",
+    "name_hero": "LINH",
+    "title": "DETECTIVE CONAN",
+    "video": "https://www.youtube.com/embed/YTSEhiEK-00",
+    "link": "https://link4m.com/XPbgpcg"
+  },
+  {
     "img": "https://lh3.googleusercontent.com/d/1N69hEo6-jAxFxK2TLPHWTUqrAuOrV0sH",
     "name_hero": "WAVE",
     "title": "FULL WAVE + PHU KIEN",
