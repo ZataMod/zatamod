97c03f106fd0ebb252b62f8184a5f8cb4fd4ebf7
ZataMod <vinhmod4@gmail.com>
2025-08-07 15:52:15 +0700

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,23 +1,2 @@
 const heroes = [
-  {
-    "img": "https://lh3.googleusercontent.com/d/1qYe6QOa695j2m3n7R0z7-3Csq5AuPDDo",
-    "name_hero": "VALHEIN",
-    "title": "THU NGUYEN VE THAN",
-    "video": "https://www.youtube.com/embed/3rrayUZVyQA",
-    "link": "https://link4m.com/Qekav"
-  },
-  {
-    "img": "https://lh3.googleusercontent.com/d/1UVna0fjwVW23dgqx1qV_kD26105KfnsW",
-    "name_hero": "LINH",
-    "title": "DETECTIVE CONAN",
-    "video": "https://www.youtube.com/embed/YTSEhiEK-00",
-    "link": "https://link4m.com/XPbgpcg"
-  },
-  {
-    "img": "https://lh3.googleusercontent.com/d/1N69hEo6-jAxFxK2TLPHWTUqrAuOrV0sH",
-    "name_hero": "WAVE",
-    "title": "FULL WAVE + PHU KIEN",
-    "video": "https://www.youtube.com/embed/TMsptx28XUY",
-    "link": "https://link4m.com/G5PfbB"
-  }
 ];
