424fd1dfaf62736c35265fd8ac0e4b8af2df0cd6
ZataMod <vinhmod4@gmail.com>
2025-08-15 01:42:59 +0700

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,9 +1,2 @@
 const heroes = [
-  {
-    "img": "https://lh3.googleusercontent.com/d/1nYa1M0kfF9yx7v7znvZv8Dcta-4oYEJE",
-    "name_hero": "WUKONG",
-    "title": "BINH MINH TAN THE",
-    "video": "https://www.youtube.com/embed/dGdoPUNf_3s",
-    "link": "https://link4m.com/2tbtoC"
-  }
 ];
