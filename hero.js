29118be6500796e8f487b44014991e0136327500
ZataMod <vinhmod4@gmail.com>
2025-08-18 20:00:28 +0700

    Thêm hero

--- hero.js
+++ hero.js
@@ -1,5 +1,10 @@
 const heroes = [
   {
+    "img": "https://vntre.vn/uploads/images/2024/03/28/rank-lien-quan-tu-thap-den-cao-1-1711592921.jpg",
+    "title": "RANK",
+    "link": "https://link4m.com/NmiOrFy"
+  },
+  {
     "img": "https://lh3.googleusercontent.com/d/1avG8ANAa2_CEjZsmaNvahrS1Ba70wn25",
     "title": "CAM XA",
     "link": "https://link4m.com/gcLoag"
