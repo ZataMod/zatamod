e064b453b32fe828f2be74bdf78ba56478d82005
ZataMod <vinhmod4@gmail.com>
2025-08-20 12:50:02 +0700

    Thêm hero

--- hero.js
+++ hero.js
@@ -1,5 +1,10 @@
 const heroes = [
   {
+    "img": "https://lh3.googleusercontent.com/d/1eyIeI9fHK6rvWL2xkp7ZnTNpYdoZTqpg",
+    "title": "NATALYA VE THAN",
+    "link": "https://link4m.com/d4xck"
+  },
+  {
     "img": "https://vntre.vn/uploads/images/2024/03/28/rank-lien-quan-tu-thap-den-cao-1-1711592921.jpg",
     "title": "RANK",
     "link": "https://link4m.com/NmiOrFy"
