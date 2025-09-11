794f84869ae2399691c11300b1b19539bbe55ae0
ZataMod <vinhmod4@gmail.com>
2025-09-11 18:38:12 +0700

    Thêm hero

--- hero.js
+++ hero.js
@@ -1,7 +1,1 @@
-const heroes = [
-  {
-    "img": "https://lh3.googleusercontent.com/d/11JabQhsz5XMLcdWNZ6T8J9p_Ohd9ujEe",
-    "title": "Capheny Bugcag Assemble",
-    "link": "https://link4m.com/XsIAw"
-  }
-];
+const heroes = [];
