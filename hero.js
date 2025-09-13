e726505d704933ab2e427940d399314fcc3e73ba
ZataMod <vinhmod4@gmail.com>
2025-09-13 10:45:05 +0700

    Thêm hero

--- hero.js
+++ hero.js
@@ -1,1 +1,8 @@
-const heroes = [];
+const heroes = [
+  {
+    "img": "https://lh3.googleusercontent.com/d/19KtGPwIOTgG2fDtuiRPvNH1Azj2DYHBx",
+    "title": "Mod Skin Azzen'ka Remake Máy Chủ Chính Thức",
+    "link": "https://link4m.com/uaDFplB",
+    "time": "09/13/2025 10:45:04"
+  }
+];
