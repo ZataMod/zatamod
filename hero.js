92f4cbdcd0c765e85fc7d665a32187f7aaccb568
ZataMod <vinhmod4@gmail.com>
2025-09-14 13:33:40 +0700

    Thêm hero

--- hero.js
+++ hero.js
@@ -1,5 +1,11 @@
 const heroes = [
   {
+    "img": "https://lh3.googleusercontent.com/d/1C57xOmC_jo-JyWVP9ET1o5UermbgwoPS",
+    "title": "Mod Skin Florentino Steve AOV × MINECRAFT",
+    "link": "https://link4m.com/8WdxS",
+    "time": "09/14/2025 13:33:39"
+  },
+  {
     "img": "https://lh3.googleusercontent.com/d/19KtGPwIOTgG2fDtuiRPvNH1Azj2DYHBx",
     "title": "Mod Skin Azzen'ka Remake Máy Chủ Chính Thức",
     "link": "https://link4m.com/uaDFplB",
